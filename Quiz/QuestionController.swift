a11afbb82d06e9d78cafa4833cff3a5b00e2d6cc
Calle Lundstedt <calle.lundstedt@gmail.com>
2016-03-08 13:03:24 +0100

    Nu går det att spara frågor

--- Quiz/QuestionController.swift
+++ Quiz/QuestionController.swift
@@ -37,8 +37,14 @@ class QuestionController: UIViewController {
     }
    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "main" {
+            
+            if let mainController = segue.destinationViewController as? ViewController {
+                
+                mainController.quiz = self.quiz!
+            }
+            
+        }
     }
     
     @IBAction func switchChanged(sender: UISwitch) {
@@ -57,5 +63,6 @@ class QuestionController: UIViewController {
         quiz?.questions.append(question)
         print(quiz?.questions.count)
     }
+    
 
 }
