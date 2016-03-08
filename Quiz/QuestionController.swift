311dab9a9d53a50543293ef3641e78bdbfeb5a4d
Calle Lundstedt <calle.lundstedt@gmail.com>
2016-03-08 11:49:05 +0100

    Fixade så bara en switch kan vara på åt gången

--- Quiz/QuestionController.swift
+++ Quiz/QuestionController.swift
@@ -9,23 +9,41 @@
 import UIKit
 
 class QuestionController: UIViewController {
+    
+    @IBOutlet weak var correctSwitch1: UISwitch!
+    @IBOutlet weak var correctSwitch2: UISwitch!
+    @IBOutlet weak var correctSwitch3: UISwitch!
+    @IBOutlet weak var correctSwitch4: UISwitch!
+    
+    var switches: [UISwitch] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        switches = [correctSwitch1,correctSwitch2,correctSwitch3,correctSwitch4]
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
+    
+    @IBAction func switchChanged(sender: UISwitch) {
+        if(sender.on) {
+            for correctSwitch in switches {
+                if(correctSwitch.on && correctSwitch != sender) {
+                    correctSwitch.setOn(false, animated: true)
+                }
+            }
+        }
+    }
+    
 
 }
